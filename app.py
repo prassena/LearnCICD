2f1623e3a7f4fd0e4f558c21f29f8d4ed61d9608
prassena <prassena.kannan@gmail.com>
2024-07-23 20:22:51 +0530

    lint10

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+"""sample flask api"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    """hello word by prassena"""
     return 'Hello, World!'
 
 if __name__ == '__main__':
