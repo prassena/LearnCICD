2c5be6bd311359cb224677fd85d58bb0ee09969d
prassena <prassena.kannan@gmail.com>
2024-07-24 16:59:07 +0530

    lint

--- test_app.py
+++ test_app.py
@@ -1,11 +1,14 @@
+"""def all"""
 from app import add, subtract
 
 def test_add():
+    """def"""
     assert add(1, 2) == 3
     assert add(-1, 1) == 0
     assert add(-1, -1) == -2
 
 def test_subtract():
+    """deffff"""
     assert subtract(2, 1) == 1
     assert subtract(2, 2) == 0
     assert subtract(-1, -1) == 0
